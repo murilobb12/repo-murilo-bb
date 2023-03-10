76b893e58af704429ec0fc3de4fae35d1edcc32d
F7287356 <murilo.asantos@bb.com.br>
2023-03-10 10:36:41 -0300

    .

--- Quarkus/quarkus-vinicius-ferraz/quarkus-vinicius-ferraz/src/test/java/br/com/murilo/AnimalResourceTest.java
+++ Quarkus/quarkus-vinicius-ferraz/quarkus-vinicius-ferraz/src/test/java/br/com/murilo/AnimalResourceTest.java
@@ -1,6 +1,9 @@
 package br.com.murilo;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,7 +18,6 @@ public class AnimalResourceTest {
 
     @Test
     public void testGetAll(){
-
         given()
                 .when()
                 .get("/animais")
@@ -24,6 +26,36 @@ public class AnimalResourceTest {
                 .body("size()", is(2), "id", hasItems(1,2));
     }
 
+    @Test
+    public void testIdNaoEncontrado(){
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/animais/3")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(404, response.statusCode());
+
+    }
+
+    @Test
+    public void testIdExistente(){
+        given()
+                .when().get("/animais/1")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void testAtualizarNomeAnimal(){
+        given().header("Content-type", "application/json")
+                .body("{\n\t\"nome\":\"Tigre\"")
+                .when().put("animais/1")
+                .then()
+                .statusCode(200);
+    }
 
 
 }
